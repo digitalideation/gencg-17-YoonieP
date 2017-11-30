d4b4ed9f492e4c70160010f30c006451144ca091
nam pham <nam.pham@stud.hslu.ch>
2017-11-30 13:37:57 +0100

    add rasterize

--- _01-Shapes/01_04/gui.js
+++ _01-Shapes/01_04/gui.js
@@ -3,11 +3,16 @@ var options = {
   tileCount: 20,
   circleFillColor: [255, 120, 0], //RGB   
   fill: false,
+  columns: 3,
+  rows: 5
 };
 
 window.onload = function() {
   var gui = new dat.GUI();
   gui.add(options, 'size').min(0).max(255).step(1);
+    gui.add(options, 'columns').min(0).max(10).step(1);
+    gui.add(options, 'rows').min(0).max(10).step(1);
   gui.add(options, 'fill');
   gui.addColor(options, 'circleFillColor');
+
 };
--- _01-Shapes/01_04/sketch.js
+++ _01-Shapes/01_04/sketch.js
@@ -4,6 +4,31 @@
 // Global var
 // The var are initialised in gui.js
 var b = 255, p = false;
+var positions = [[]];
+var bla= true;
+
+function raseterize(){
+  if (bla == true) {
+  let columnlength = windowWidth / options.columns;
+  let rowlength = windowHeight / options.rows;
+
+  for (var columnval = 0; columnval < options.columns; columnval++) 
+  {
+    let test =[]
+    for (var rowval = 0; rowval < options.rows; rowval++) 
+    { 
+      let position = []
+      position.push(columnlength * (columnval + 1));// X
+      position.push(rowlength * ( rowval + 1));// Y
+      test.push(position);
+    }
+      positions.push(test);
+  }
+
+  bla= false;
+  }
+
+}
 
 function setup() {
   // Canvas setup
@@ -16,18 +41,47 @@ function setup() {
   // The var are initialised in gui.js
 
   background(255);
+/*
+  maxDistance = dist(windowWidth/2, windowHeight/2, windowWidth, windowHeight);
+  for (var x = 0; x < windowWidth; x++) {
+    distances[x] = []; // create nested array
+    for (var y = 0; y < windowHeight; y++) {
+      var distance = dist(windowWidth/2, windowHeight/2, x, y);
+      distances[x][y] = distance/maxDistance * 255;
+    }
+  }
+  spacer = 10;
+  */
   smooth();
   // Init Var
 }
 
 function draw() {
    smooth();
+  options.circleFillColor[2] = random(255);
+  raseterize();
+
   noFill();
   if (p) {
-    b = options.circleFillColor;
-    push();
+background(0);
+    for (var i = 1, len = positions.length; i < len; i++) {
+      for (var b = 0, cen = positions[i].length; b < cen; b++) {
+        xVal = positions[i][b][0];
+        yVal = positions[i][b][1];
+        //console.log("valuepair:")
+        //console.log("x :" + xVal);
+        //console.log("y :" + yVal);
 
-    translate(width / 2, height / 2);
+        drawCircle(xVal,yVal);
+      }
+    }
+  }
+function drawCircle(posX, posY){
+   b = options.circleFillColor;
+    push();
+    console.log(posX);
+    console.log(posY);
+    translate(posX, posY);
 
     var circleResolution = toInt(map(mouseY + 100, 0, height, 2, 10));
     var radius = mouseX - width / 2 + 0.5;
@@ -45,7 +99,8 @@ function draw() {
     endShape();
 
     pop();
-  }
+}
+
 /*
   translate(width/options.tileCount/2, height/options.tileCount/2);
 
