38ebedfbfa0dc2c5487160c35e487f67ed685145
nam pham <nam.pham@stud.hslu.ch>
2017-12-14 15:52:33 +0100

    blub

--- _01-Shapes/01_08/gui.js
+++ _01-Shapes/01_08/gui.js
@@ -5,7 +5,5 @@ var options = {
 window.onload = function() {
   var gui = new dat.GUI();
   gui.add(options, 'sizeNoise').min(0).max(2000).step(10);
-  gui.add(options, 'multiplikatorSize').min(0).max(250).step(25);
-;
 
 };
